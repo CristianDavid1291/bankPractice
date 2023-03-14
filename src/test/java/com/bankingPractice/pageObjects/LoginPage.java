8ad6646233bce57e1ec64be0d62f96f210b53773
Cristian David <cristiandavid1291@gmail.com>
2023-03-14 11:11:00 -0500

    validate field in data provider

--- src/test/java/com/bankingPractice/pageObjects/LoginPage.java
+++ src/test/java/com/bankingPractice/pageObjects/LoginPage.java
@@ -1,55 +1,67 @@
 package com.bankingPractice.pageObjects;
 
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
-	
+
 	WebDriver driver;
-	
+
 	@FindBy(css = "input[name='uid']")
 	WebElement nameField;
-	
+
 	@FindBy(css = "input[name='password']")
 	WebElement passwordField;
-	
+
 	@FindBy(css = "input[name='btnLogin']")
 	WebElement loginButton;
-	
+
 	@FindBy(css = "input[name='btnLogin']")
 	WebElement resetButton;
-	
+
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	public void setName(String name) {
 		nameField.sendKeys(name);
 	}
-	
+
 	public void setPassword(String password) {
 		passwordField.sendKeys(password);
 	}
-	
+
 	public void clickLoginButton() {
 		loginButton.click();
 	}
-	
+
 	public void clickresetButton() {
 		resetButton.click();
 	}
-	
+
 	public HomePage logOut() {
 		HomePage hm = new HomePage(driver);
 		hm.clickLogOutButton();
-		driver.switchTo().alert().accept();
-		driver.switchTo().defaultContent();
-		return hm;
+		if (isAlertPresent()) {
+			driver.switchTo().alert().accept();
+			driver.switchTo().defaultContent();
+			return hm;
+		} else {
+			return null;
+		}
 	}
 
-	
+	public boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 
 }
--- src/test/java/com/bankingPractice/testCases/TC_LoginTest_001.java
+++ src/test/java/com/bankingPractice/testCases/TC_LoginTest_001.java
@@ -11,26 +11,40 @@ import com.bankingPractice.utilities.Provider;
 public class TC_LoginTest_001 extends Base {
 
 	@Test(dataProvider = "getData", dataProviderClass = Provider.class)
-	public void test_001(String userP,String passwordP, String expectationP) {
-		
+	public void test_001(String userP, String passwordP, String expectationP) {
+
 		logger.info("URL is opened");
-		
+
 		LoginPage lp = new LoginPage(driver);
 		lp.setName(userP);
 		logger.info("user name entered" + userP);
 		lp.setPassword(passwordP);
-		logger.info("password entered"+ passwordP);
+		logger.info("password entered" + passwordP);
 		lp.clickLoginButton();
 		logger.info("Login button clicked");
 		logger.info("Validating page tittle");
-		if (driver.getTitle().equals("Guru99 Bank Manager HomePage")) {
-			Assert.assertTrue(true);
-			logger.info("Login test passed");
+
+		if (expectationP.equals("valid")) {
+			String title = driver.getTitle();
+			Assert.assertEquals(title, "Guru99 Bank Manager HomePage");
+			HomePage hm = lp.logOut();
+
+			if (hm == null) {
+				logger.warn("Alert not found");
+				Assert.assertTrue(false, " Alert not found");
+
+			}
+			
 		} else {
-			Assert.assertTrue(false);
-			logger.info("Login test failed");
+
+			String message = driver.switchTo().alert().getText();
+			driver.switchTo().alert().accept();
+			driver.switchTo().defaultContent();
+			logger.info(message);
+			Assert.assertEquals(message,"User is not valid");
+
 		}
-		HomePage hm = lp.logOut();
+
 	}
 
 }
